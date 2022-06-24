e4899f6c7832ce74ac466ab243bca36385884bca
Reyner Veliz <reynerv@yahoo.com>
2022-06-24 15:49:22 -0600

    Create basic express app

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+const port = 3000;
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req, res) => {
+    // res.send('Hello World!');
+    res.render('home');
+});
+
+app.listen(port, () => {
+    console.log(`App listening at http://localhost:${port}`);
+});
